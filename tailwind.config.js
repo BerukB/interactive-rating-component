288af5beac1241b4296da791a499711ecf30f5b0
BerukB <berukberhanua@gmail.com>
2023-08-16 13:53:22 -0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        white: "hsl(0, 0%, 100%)",
+        lightGrey: "hsl(217, 12%, 63%)",
+        mediumGrey: "hsl(216, 12%, 54%)",
+        darkBlue: " hsl(213, 19%, 18%)",
+        veryDarkBlue: "hsl(216, 12%, 8%)",
+      },
+    },
+  },
+  plugins: [],
+};
